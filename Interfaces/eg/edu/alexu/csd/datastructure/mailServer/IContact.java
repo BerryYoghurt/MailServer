30b04511804c68f55c0f45c97e634e46fef7dcd8
ManarAmr25 <59005827+ManarAmr25@users.noreply.github.com>
2020-04-22 21:59:45 +0200

    Add files via upload

--- Interfaces/eg/edu/alexu/csd/datastructure/mailServer/IContact.java
+++ Interfaces/eg/edu/alexu/csd/datastructure/mailServer/IContact.java
@@ -1,18 +1,17 @@
 package eg.edu.alexu.csd.datastructure.mailServer;
 
-/**
- * creates a new contact instance that can either be added to the
- * database as a new account or added into a user's phonebook 
- * 
- * Preferrably, we should create 2 classes of this interface, one for
- * new account and one for contact*/
+import java.io.File;
 
 public interface IContact {
 	/**
-	 * sets (or adds) the email address of the conact and validates it depending on whether
+	 * @return contact/user folder path
+	 */
+	public File getPath();
+	/**
+	 * sets (or adds) the email address of the contact and validates it depending on whether
 	 * it's a new account or an existing user
 	 * @param address new email address
-	 * @return whether the process has been "successfull"*/
+	 * @return whether the process has been "successful"*/
 	public boolean setAddress(String address);
 	/**
 	 * @return the address (if an account) or addresses(if a contact)*/
@@ -36,7 +35,7 @@ public interface IContact {
 	 * sets the name of the contact
 	 * @param name
 	 * @return true if success*/
-	public boolean setName(String name);
+	public boolean setName(String Fname,String Lname);
 	/**
 	 * @return name*/
 	public String getName();
@@ -50,7 +49,7 @@ public interface IContact {
 	 * @param contact 
 	 * 		new contact
 	 * @return 1 iff added and no duplicates, 0 if duplicate, -1 if other error*/
-	public int appendIndex(IIndex indexFile);
+	public int appendIndex(IFolder indexFile);
 	/**
 	 * @return draft folder*/
 	public IFolder getDraftPath();
