511d7030c8b3884515a42b3c039c92bcae00226f
BerryYoghurt <jehadaly@gmail.com>
2020-05-13 01:34:41 +0200

    merge

--- Classes/eg/edu/alexu/csd/datastructure/mailServer/nameEdit.java
+++ Classes/eg/edu/alexu/csd/datastructure/mailServer/nameEdit.java
@@ -19,70 +19,64 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 
-public class nameEdit extends JFrame {
+public class NameEdit extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
-    private App app;
+	private App app;
 	/**
 	 * Launch the application.
 	 */
-	/*public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					nameEdit frame = new nameEdit();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
+	/*
+	 * public static void main(String[] args) { EventQueue.invokeLater(new
+	 * Runnable() { public void run() { try { nameEdit frame = new nameEdit();
+	 * frame.setVisible(true); } catch (Exception e) { e.printStackTrace(); } } });
+	 * }
+	 */
 
 	/**
 	 * Create the frame.
 	 */
-	public nameEdit(App app) {
+	public NameEdit(App app, JLabel nameLabel) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 369, 236);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("First Name ");
 		lblNewLabel.setForeground(new Color(0, 139, 139));
 		lblNewLabel.setFont(new Font("Century Gothic", Font.PLAIN, 21));
 		lblNewLabel.setBounds(38, 32, 113, 29);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblLastName = new JLabel("Last Name ");
 		lblLastName.setForeground(new Color(0, 139, 139));
 		lblLastName.setFont(new Font("Century Gothic", Font.PLAIN, 21));
 		lblLastName.setBounds(38, 89, 113, 29);
 		contentPane.add(lblLastName);
-		
+
 		textField = new JTextField();
 		textField.setBounds(161, 32, 153, 29);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(161, 89, 153, 29);
 		contentPane.add(textField_1);
-		
+
 		JButton btnNewButton = new JButton("Confirm");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(app.signedInUser.setName(textField.getText(), textField_1.getText())) {
-			   app.signedInUser.writeToFile();
-			   dispose();
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "Invalid Name");	
+				if (app.signedInUser.setName(textField.getText(), textField_1.getText())) {
+					app.signedInUser.writeToFile();
+					nameLabel.setText(app.signedInUser.getName());
+					dispose();
+				} else {
+					JOptionPane.showMessageDialog(null, "Invalid Name");
 				}
 			}
 		});
@@ -91,7 +85,7 @@ public class nameEdit extends JFrame {
 		btnNewButton.setFont(new Font("Century Gothic", Font.PLAIN, 20));
 		btnNewButton.setBounds(48, 147, 113, 29);
 		contentPane.add(btnNewButton);
-		
+
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.setBackground(new Color(255, 255, 255));
 		btnCancel.setForeground(new Color(0, 206, 209));
--- Classes/eg/edu/alexu/csd/datastructure/mailServer/passwordEdit.java
+++ Classes/eg/edu/alexu/csd/datastructure/mailServer/passwordEdit.java
@@ -1,54 +1,46 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
 
 import eg.edu.alexu.csd.datastructure.mailServer.App;
+import javax.swing.SwingConstants;
 
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
-import java.awt.Font;
-import javax.swing.JPasswordField;
-import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-public class passwordEdit extends JFrame {
+public class PasswordEdit extends JFrame {
 
 	private JPanel contentPane;
-	private final JLabel CurrentPassword = new JLabel("Current PassWord");
+	private final JLabel CurrentPassword = new JLabel("Current Password");
 	private JPasswordField passwordField;
 	private JPasswordField passwordField_1;
 	private JPasswordField passwordField_2;
-  private App app;
+	private App app;
 
 	/**
 	 * Launch the application.
 	 */
-	/*public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					passwordEdit frame = new passwordEdit(app);
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
+	/*
+	 * public static void main(String[] args) { EventQueue.invokeLater(new
+	 * Runnable() { public void run() { try { PasswordEdit frame = new
+	 * PasswordEdit(app); frame.setVisible(true); } catch (Exception e) {
+	 * e.printStackTrace(); } } }); }
+	 */
 
 	/**
 	 * Create the frame.
 	 */
-	public passwordEdit(App app) {
-  	this.app = app;
+	public PasswordEdit(App app) {
+		setTitle("Password edit");
+		this.app = app;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 521, 357);
 		contentPane = new JPanel();
@@ -59,46 +51,46 @@ public class passwordEdit extends JFrame {
 		CurrentPassword.setFont(new Font("Century Gothic", Font.BOLD, 16));
 		CurrentPassword.setBounds(56, 28, 135, 36);
 		contentPane.add(CurrentPassword);
-		
-		JLabel lblNewPassword = new JLabel("New PassWord");
+
+		JLabel lblNewPassword = new JLabel("New Password");
+		lblNewPassword.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewPassword.setForeground(new Color(0, 139, 139));
 		lblNewPassword.setFont(new Font("Century Gothic", Font.BOLD, 16));
 		lblNewPassword.setBounds(56, 88, 135, 36);
 		contentPane.add(lblNewPassword);
-		
-		JLabel lblConfirmPassword = new JLabel("Confirm PassWord");
+
+		JLabel lblConfirmPassword = new JLabel("Confirm Password");
 		lblConfirmPassword.setForeground(new Color(0, 139, 139));
 		lblConfirmPassword.setFont(new Font("Century Gothic", Font.BOLD, 16));
 		lblConfirmPassword.setBounds(56, 147, 140, 36);
 		contentPane.add(lblConfirmPassword);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(233, 34, 216, 31);
 		contentPane.add(passwordField);
-		
+
 		passwordField_1 = new JPasswordField();
 		passwordField_1.setBounds(233, 94, 216, 31);
 		contentPane.add(passwordField_1);
-		
+
 		passwordField_2 = new JPasswordField();
 		passwordField_2.setBounds(233, 152, 216, 31);
 		contentPane.add(passwordField_2);
-		
+
 		JButton ok = new JButton("Confirm");
 		ok.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-					if (!passwordField_1.getText().equals(passwordField_2.getText())) {
-          	JOptionPane.showMessageDialog(null, "passwords do not match");
-          }
-          if(!app.signedInUser.matchPassword(passwordField.getText())){
-          	JOptionPane.showMessageDialog(null, "wrong password");
-          }
-         else if (!app.signedInUser.setPassword(passwordField_1.getText())){
-           JOptionPane.showMessageDialog(null, "Invalid password");
-         } else {
-        	    app.signedInUser.writeToFile();
-         		dispose();
-         }
+				if (!passwordField_1.getText().equals(passwordField_2.getText())) {
+					JOptionPane.showMessageDialog(null, "passwords do not match");
+				}
+				if (!app.signedInUser.matchPassword(passwordField.getText())) {
+					JOptionPane.showMessageDialog(null, "wrong password");
+				} else if (!app.signedInUser.setPassword(passwordField_1.getText())) {
+					JOptionPane.showMessageDialog(null, "Invalid password");
+				} else {
+					app.signedInUser.writeToFile();
+					dispose();
+				}
 			}
 		});
 		ok.setBackground(new Color(255, 255, 255));
@@ -106,7 +98,7 @@ public class passwordEdit extends JFrame {
 		ok.setForeground(new Color(0, 206, 209));
 		ok.setBounds(106, 244, 107, 36);
 		contentPane.add(ok);
-		
+
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -119,4 +111,5 @@ public class passwordEdit extends JFrame {
 		btnCancel.setBounds(277, 244, 107, 36);
 		contentPane.add(btnCancel);
 	}
+
 }
