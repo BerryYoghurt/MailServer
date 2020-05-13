4185c49e00202cc33a0edc6bdb5da3a1c4279a63
ManarAmr25 <59005827+ManarAmr25@users.noreply.github.com>
2020-05-13 03:28:18 +0200

    Add files via upload

--- Classes/eg/edu/alexu/csd/datastructure/mailServer/SenderFilter.java
+++ Classes/eg/edu/alexu/csd/datastructure/mailServer/SenderFilter.java
@@ -6,41 +6,38 @@ import java.nio.file.Paths;
 
 import eg.edu.alexu.csd.datastructure.linkedList.Classes.DLinkedList;
 
-
 public class SenderFilter implements IFilter {
 
-	File folder;
-	private String field;
+	File folder; // directory of the folder which the filtered items are directed to
+	private String field; // all/part of the sender name which needs to be in all filtered items
 
-	SenderFilter(File path) {
+	public SenderFilter(File path) {
 		this.folder = path;
 	}
 
 	@Override
 	public DLinkedList applyFilter(DLinkedList list) {
+		if (field == null || list == null || folder == null) return null;
 		DLinkedList filtered = new DLinkedList();
-		for(Object o : list) {// add clone in MailInfo
+		for (Object o : list) {// add clone in MailInfo
 			MailInfo item = (MailInfo) o;
-			while (item != null) {
-				if (item.sender.contains(field)) {
-					filtered.add(item);
-					String newDirectory = this.folder.getAbsolutePath() + Paths.get(item.directory).getFileName();
-					try {
-						MailFolder.copyFolder(new File(item.directory), new File(newDirectory));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					item.directory = newDirectory;
+			if (item.sender.contains(field)) {
+				filtered.add(item);
+				String newDirectory = this.folder.getAbsolutePath() + Paths.get(item.directory).getFileName();
+				try {
+					MailFolder.copyFolder(new File(item.directory), new File(newDirectory));
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
+				//item.directory = newDirectory;
 			}
+
 		}
 		return filtered;
 	}
 
-	
 	public void setParameter(String parameter) {
-		this.field = parameter;		
+		this.field = parameter;
 	}
 
 }
--- Classes/eg/edu/alexu/csd/datastructure/mailServer/SubjectFilter.java
+++ Classes/eg/edu/alexu/csd/datastructure/mailServer/SubjectFilter.java
@@ -11,37 +11,36 @@ import java.util.Comparator;
 import eg.edu.alexu.csd.datastructure.linkedList.Classes.DLinkedList;
 
 public class SubjectFilter implements IFilter {
-	
-	File folder;
-	private String field;
 
-	SubjectFilter(File path) {
+	File folder; // directory of the folder which the filtered items are directed to
+	private String field; // all/part of the subject which needs to be in all filtered items
+
+	public SubjectFilter(File path) {
 		this.folder = path;
 	}
 
 	@Override
-	public DLinkedList applyFilter(DLinkedList list) { // linked list of info
+	public DLinkedList applyFilter(DLinkedList list) { // linked list of MailInfo
+		if (field == null || list == null || folder == null) return null;
 		DLinkedList filtered = new DLinkedList();
-		for(Object o : list) {// add clone in MailInfo
+		for (Object o : list) {
 			MailInfo item = (MailInfo) o;
-			while (item != null) {
-				if (item.subject.contains(field)) {
-					filtered.add(item);
-					String newDirectory = this.folder.getAbsolutePath() + Paths.get(item.directory).getFileName();
-					try {
-						MailFolder.copyFolder(new File(item.directory), new File(newDirectory));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					item.directory = newDirectory;
+			if (item.subject.contains(field)) {
+				filtered.add(item);
+				String newDirectory = this.folder.getAbsolutePath() + Paths.get(item.directory).getFileName();
+				try {
+					MailFolder.copyFolder(new File(item.directory), new File(newDirectory));
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
+				// item.directory = newDirectory;
 			}
+
 		}
 		return filtered;
 	}
 
 	public void setParameter(String parameter) {
-		this.field = parameter;		
+		this.field = parameter;
 	}
 }
